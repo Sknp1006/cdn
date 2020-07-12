1a9e4d045e9c09e75e460e65537f09778513f796
Sknp1006 <41496773+Sknp1006@users.noreply.github.com>
2020-07-12 18:40:22 +0800

    明日方舟背景图

--- img/formatting.py
+++ img/formatting.py
@@ -1,12 +1,26 @@
 import glob
+import os
 
-img_list = glob.glob(r"C:\Users\SKNP\Documents\GitHub\cdn\img\arknights/*.jpg")
-print(img_list)
-with open('albums.txt', mode='w') as f:
-    for img in img_list:
-        caption = img.split("\\")[-1]
-        src = "https://cdn.jsdelivr.net/gh/Sknp1006/cdn/img/arknights/" + caption
+dir_name = input("请输入目标文件夹：")
+source_path = r"C:\Users\SKNP\Documents\GitHub\cdn\img"
 
-        f.writelines("- caption: " + caption + '\n')
-        f.writelines("  src: " + src + '\n')
-
+if dir_name == 'albums':
+    sub_dir = os.listdir("\\".join([source_path, dir_name]))
+    glob_path = {sub: "\\".join([source_path, dir_name, sub, "*[jpg,png]"]) for sub in sub_dir}
+    for sub in glob_path:
+        img_list = glob.glob(glob_path[sub], recursive=True)
+        with open('{}.txt'.format(sub), mode='w') as f:
+            for img in img_list:
+                caption = img.split("\\")[-1]
+                src = "https://cdn.jsdelivr.net/gh/Sknp1006/" + '/'.join(img.split("\\")[5:])
+                f.writelines("- caption: " + caption + '\n')
+                f.writelines("  src: " + src + '\n')
+else:
+    glob_path = {dir_name: "\\".join([source_path, dir_name, "*[jpg,png]"])}
+    img_list = glob.glob(glob_path[dir_name], recursive=True)
+    with open('{}.txt'.format(dir_name), mode='w') as f:
+        for img in img_list:
+            caption = img.split("\\")[-1]
+            src = "https://cdn.jsdelivr.net/gh/Sknp1006/" + '/'.join(img.split("\\")[5:])
+            f.writelines("- caption: " + caption + '\n')
+            f.writelines("  src: " + src + '\n')
